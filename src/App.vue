ad8632a491135bb2923fb686fb069b68e47fba67
Theokleses <osahonschmolze222@gmail.com>
2025-09-08 15:49:19 +0200

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <div id="app">
+  <div >
+    <BaseCard>
+      <h2 style="color: white">Titel</h2>
+      <p style="color: #ccc">Das ist Inhalt in der Card.</p>
+    </BaseCard>
   </div>
 </template>
 
 <script>
+import BaseCard from "./components/BaseCard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
+    BaseCard,
   }
 }
 </script>
-
 
 <style>
 body {
@@ -19,8 +24,10 @@ body {
 }
 
 #app {
-  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
+  background: radial-gradient(71.1% 100% at 50% 0%,#020204 14.6%,#011f35 100%);
   width: 100vw;
   min-height: 100vh;
+  padding: 100px;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<!-- src/components/Card.vue -->
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default{
+  name: "BaseCard",
+}
+</script>
+
+<style scoped>
+.card {
+  background-color: #011F35;
+  border-radius: 16px;
+  padding: 24px 32px;
+}
+</style>
