ec96755e01cf712c3ec93e65f18bb1607f8047df
Theokleses <osahonschmolze222@gmail.com>
2025-09-09 15:40:57 +0200

    Added stockService and revenue shown in console sorted from first to last

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,17 @@
 
 <script>
 import BaseCard from "./components/BaseCard.vue";
+import { stockService } from "@/services/stockService";
 
 export default {
   name: "App",
   components: {
-    BaseCard,
+    BaseCard
+  },
+  async created() {
+  this.data = await stockService.getRevenue('$AAPL');
+  console.log('Loaded data', this.data);
+      
   }
 }
 </script>
